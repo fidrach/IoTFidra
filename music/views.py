93aba0091def5ba3eaf256502995b0e5cf5fc87d
Alexis <alexischeungho@gmail.com>
2017-07-03 16:39:32 -0700

    Added forms to details

--- music/views.py
+++ music/views.py
@@ -1,16 +1,17 @@
-from django.http import Http404
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from .models import Album
+from .models import Song
 
-# Create your views here.
 def index(request):
     all_albums = Album.objects.all()
 
     context = {
         "all_albums": all_albums,
     }
+
     return render(request, "music/index.html", context)
+
 
 def detail(request, album_id):
     album = get_object_or_404(Album, id=album_id)
@@ -18,4 +19,23 @@ def detail(request, album_id):
     context = {
         "album": album,
     }
+
+    return render(request, "music/detail.html", context)
+
+
+def favorite(request, album_id):
+    album = get_object_or_404(Album, id=album_id)
+
+    context = {
+        "album": album,
+    }
+
+    try:
+        selected_song = album.song_set.get(id=request.POST["song"])
+    except (KeyError, Song.DoesNotExist):
+        context["error_message"] = "You did not select a valid song"
+    else:
+        selected_song.is_favorite = True
+        selected_song.save()
+
     return render(request, "music/detail.html", context)
