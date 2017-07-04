1fb6fbc55d2cb47135885bef6d8673008bc5153a
Alexis <alexischeungho@gmail.com>
2017-07-03 20:55:27 -0700

    Tutorial 29

--- music/views.py
+++ music/views.py
@@ -21,21 +21,3 @@ def detail(request, album_id):
     }
 
     return render(request, "music/detail.html", context)
-
-
-def favorite(request, album_id):
-    album = get_object_or_404(Album, id=album_id)
-
-    context = {
-        "album": album,
-    }
-
-    try:
-        selected_song = album.song_set.get(id=request.POST["song"])
-    except (KeyError, Song.DoesNotExist):
-        context["error_message"] = "You did not select a valid song"
-    else:
-        selected_song.is_favorite = True
-        selected_song.save()
-
-    return render(request, "music/detail.html", context)
